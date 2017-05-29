cfce2a3d3ecd23ffca8843c351eb4f39b02e0ae2
Dustin Walker <dwtimp@gmail.com>
2017-05-29 16:03:21 -0500

    making styling changes

---
+++ app/transitions.js
@@ -0,0 +1,8 @@
+// export default function(){
+//   this.transition(
+//     this.fromRoute('application'),
+//     this.toRoute('resume'),
+//     this.use('toLeft'),
+//     this.reverse('toRight')
+//   );
+// };
